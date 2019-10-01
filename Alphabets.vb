53e90372f8785cd9db4c0926867068eb611f6f3c
Muhammmad Ahmed <55909145+ahmed351@users.noreply.github.com>
2019-10-01 20:36:02 +0500

    Add files via upload
    visual basic

---
+++ Alphabets.vb
@@ -0,0 +1,34 @@
+﻿Module Alphabets
+
+    Sub Main()
+        Dim Str1 As String
+        Dim counter, alpha, Noalpha As Integer
+        Dim letter As String
+
+
+        Str1 = ""
+        letter = ""
+        counter = 0
+        alpha = 0
+        Noalpha = 0
+
+        Console.Write("Input String: ")
+        Str1 = Console.ReadLine
+
+
+        For counter = 1 To Len(Str1)
+            letter = Mid(Str1, counter, 1)
+            If LCase(letter) < "a" Or LCase(letter) > "z" Then
+                Noalpha = Noalpha + 1
+            Else
+                alpha = alpha + 1
+            End If
+        Next
+
+        Console.WriteLine(alpha, "Alphabets")
+        Console.WriteLine(Noalpha, "Non Alphabets")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
