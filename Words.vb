2e4255c517d63cb67308bc41788e82c038a80770
Muhammmad Ahmed <55909145+ahmed351@users.noreply.github.com>
2019-10-01 20:01:31 +0500

    Add files via upload
    words visual basic

---
+++ Words.vb
@@ -0,0 +1,35 @@
+﻿Module Words
+
+    Sub Main()
+        Dim S1, S2, firstword, secword As String
+        Dim SP As Integer
+
+        S1 = ""
+        S2 = ""
+        SP = 0
+        firstword = ""
+        secword = ""
+        Console.Write("Fill First String: ")
+        S1 = Console.ReadLine
+        Console.Write("Fill Second String: ")
+        S2 = Console.ReadLine
+
+        SP = InStr(S1, " ")
+        firstword = Left(S1, SP - 1)
+
+        SP = InStr(S2, " ")
+        secword = Right(S2, Len(S2) - SP)
+
+        Console.Write("Final Sring = " & firstword & " " & secword)
+
+        Console.ReadKey()
+
+
+
+
+
+
+
+    End Sub
+
+End Module
