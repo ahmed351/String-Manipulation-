5c634516242876db0ea6b2d3103b98a096cd308e
Muhammmad Ahmed <55909145+ahmed351@users.noreply.github.com>
2019-10-02 19:36:35 +0500

    Add files via upload

---
+++ Reverse.vb
@@ -0,0 +1,20 @@
+﻿Module reverse
+
+    Sub Main()
+        Dim count As Integer
+
+        For count = 100 To 1 Step -1
+
+            Console.WriteLine(count)
+        Next
+
+
+        Console.ReadKey()
+
+
+
+
+
+    End Sub
+
+End Module
